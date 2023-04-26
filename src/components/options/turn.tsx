24d6a43c80bea829faa50f3c3a8b229c6c07ebaa
ali navidi <ali_navidi24@yahoo.com>
2023-04-26 13:28:24 +0330

    Style changed

--- src/components/options/turn.tsx
+++ src/components/options/turn.tsx
@@ -1,6 +1,6 @@
 const Turn = () => {
   return (
-    <div className="turn bg-slate-900 rounded-md shadow-2xl flex justify-center items-center text-white  p-1 pr-2 pl-2 gap-1">
+    <div className="turn bg-slate-900 rounded-md shadow-2xl flex justify-center items-center text-white  p-1 px-5 gap-1">
       <p className="font-semibold">&#10008;</p>
       <p className="text-xs">TURN</p>
     </div>
