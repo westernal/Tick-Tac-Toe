d2c149cf0c18c60a1611d7758c4418830b274e45
ali navidi <ali_navidi24@yahoo.com>
2023-04-26 13:27:22 +0330

    Redux added

--- src/components/Stats/o.tsx
+++ src/components/Stats/o.tsx
@@ -1,11 +1,16 @@
+import { useSelector } from "react-redux";
+import { Stats } from "../../types";
+
 const OStats = () => {
+  const numOfLoss = useSelector((state: Stats) => state.loss);
+
   return (
     <div className="turn bg-amber-300 rounded-md shadow-2xl text-center text-white  p-1 pr-2 pl-2  grid place-items-center">
       <div className="flex justify-center gap-1 items-center">
         <p className="font-semibold">O</p>
         <p className="text-xs">{"(CPU)"}</p>
       </div>
-      <div className="number mt-1 text-sm">0</div>
+      <div className="number mt-1 text-sm">{numOfLoss}</div>
     </div>
   );
 };
--- src/components/Stats/x.tsx
+++ src/components/Stats/x.tsx
@@ -1,11 +1,16 @@
+import { useSelector } from "react-redux";
+import { Stats } from "../../types";
+
 const XStats = () => {
+  const numOfWins = useSelector((state: Stats) => state.wins);
+
   return (
     <div className="turn bg-red-500 rounded-md shadow-2xl  text-white  p-1 pr-2 pl-2 text-center  grid place-items-center">
       <div className="flex justify-center gap-1 items-center">
         <p className="font-semibold">&#10008;</p>
         <p className="text-xs">{"(YOU)"}</p>
       </div>
-      <div className="number mt-1 text-sm">0</div>
+      <div className="number mt-1 text-sm">{numOfWins}</div>
     </div>
   );
 };
